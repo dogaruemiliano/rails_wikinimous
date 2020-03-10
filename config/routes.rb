400590f2eda18aa655aebdd4e56577f5576992b5
Emiliano Dogaru <dogaru.emiliano@gmail.com>
2020-03-10 17:11:35 +0100

    :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read All
+  get '/articles', to: 'articles#index', as: :tasks
+
+  # Create
+  # get '/articles/new', to: 'articles#new', as: :new_task
+  # post '/articles', to: 'articles#create'
+
+  # Read One
+  # get '/articles/:id', to: 'articles#show', as: :task
+
+  # Update
+  # get '/articles/:id/edit', to: 'articles#edit'
+  # patch '/articles', to: "articles#update"
+
+  # Delete
+  # delete '/articles/:id', to: 'articles#destroy'
 end
